78922b0f63290807cdf43fe950cc0f762a8eb9b5
cladttt <cladtsai@gmail.com>
2024-01-11 22:24:18 +0800

    hello cladtttastro

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import mdx from '@astrojs/mdx';
+import react from "@astrojs/react";
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://clad000.github.io',
+  integrations: [mdx(), sitemap(), react({
+    include: ['**/react/*'],
+    experimentalReactChildren: true
+  }), tailwind()]
+});
